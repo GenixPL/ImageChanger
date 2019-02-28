5174b84a97140b6e89ebb384837ef58e4493ea85
genixpl <intergenixyt@gmail.com>
2019-02-28 11:53:43 +0100

    add import, default and save

--- app/src/main/java/com/genix/imagechanger/MainActivity.kt
+++ app/src/main/java/com/genix/imagechanger/MainActivity.kt
@@ -1,40 +1,122 @@
 package com.genix.imagechanger
 
+import android.app.Activity
 import android.content.Intent
-import android.graphics.drawable.Drawable
-import android.media.Image
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
+import android.os.Environment
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
-import android.widget.ImageView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
+
+const val IMAGE_PICK_CODE = 10
+const val WRITE_REQUEST_CODE = 20
 
 class MainActivity : AppCompatActivity() {
 
-//	private var defaultImage: Drawable = getDrawable(R.drawable.file_image)
+	private var defaultImage: Bitmap? = null
+	private var currentImage: Bitmap? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        supportActionBar!!.hide()
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+		supportActionBar!!.hide()
 
-	    initButtons()
-    }
+		initButtons()
+	}
 
 	private fun initButtons() {
-		this.importButton.setOnClickListener { importAndSetImage() }
+		importButton.setOnClickListener { importImage() }
+		saveButton.setOnClickListener { saveImageToInternalStorage() }
+		setDefaultButton.setOnClickListener { setCurrentImageAsDefault() }
+		importDefaultButton.setOnClickListener { setDefaultAsCurrent() }
 	}
 
-	private fun importAndSetImage() {
+	private fun importImage() {
 		val intent = Intent(Intent.ACTION_GET_CONTENT)
 		intent.type = "image/*"
+
 		if (intent.resolveActivity(packageManager) != null) {
-			startActivityForResult(intent, 0)
+			startActivityForResult(intent, IMAGE_PICK_CODE)
 		}
 	}
-	
+
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		if (requestCode == IMAGE_PICK_CODE && resultCode == Activity.RESULT_OK) {
+
+			var imageUri: Uri? = data?.data
+			currentImage = MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri)
+
+			setImage()
+		}
+
+		super.onActivityResult(requestCode, resultCode, data)
+	}
+
+	private fun setImage() {
+		imageView.setImageBitmap(currentImage)
+	}
+
+	private fun setCurrentImageAsDefault() {
+		if (currentImage == null) {
+			toast("There is no image currently")
+		} else {
+			defaultImage = currentImage
+		}
+	}
+
+	private fun setDefaultAsCurrent() {
+		if (defaultImage == null) {
+			toast("There is no default image")
+		} else {
+			currentImage = defaultImage
+			setImage()
+		}
+	}
+
+	private fun saveImageToInternalStorage() {
+		ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), WRITE_REQUEST_CODE)
+
+		if (currentImage == null) {
+			toast("There is no image to save")
+			return
+		}
+
+		val root = Environment.getExternalStorageDirectory().toString()
+		val myDir = File("$root/saved_photos")
+		myDir.mkdirs()
+
+		val timeStamp = SimpleDateFormat("yyyyMMddHHmmss").format(Date())
+		var file = File(myDir, "$timeStamp.jpg")
+
+
+		try {
+			val stream: OutputStream = FileOutputStream(file)
+
+			currentImage?.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+
+			stream.flush()
+			stream.close()
+
+		} catch (e: IOException) {
+			e.printStackTrace()
+		}
+	}
+
+
+
+
+	/* OTHER METHODS */
 	private fun toast(message: String) {
 		Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 	}

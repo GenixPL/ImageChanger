a115d06894c0cea6530764cf11cbee5ae942bd85
Łukasz Niedziałek <intergenixyt@gmail.com>
2019-03-03 03:39:47 +0100

    Blur works

--- app/src/main/java/com/genix/imagechanger/ConvolutionFiltersActivity.kt
+++ app/src/main/java/com/genix/imagechanger/ConvolutionFiltersActivity.kt
@@ -8,19 +8,64 @@ import android.renderscript.Allocation
 import android.renderscript.Element
 import android.renderscript.RenderScript
 import android.renderscript.Type
+import android.util.Half.toFloat
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_convolution_filters.*
-import kotlinx.android.synthetic.main.activity_functional_filters.*
 
 
 class ConvolutionFiltersActivity : AppCompatActivity() {
 
+	/**
+	 * Context used in functions related to RenderScript
+	 */
 	private var rsContext : RenderScript? = null
+
+	/**
+	 * Array of all pixels of current bitmap from MainActivity
+	 */
 	private var pixels : IntArray? = null
-	private var divisors: FloatArray? = FloatArray(9) { 0.11f }
+
+	/**
+	 * Array of weights assigned to each cell of matrix
+	 *
+	 * @default: 0.11 (1/9 - equally weighted)
+	 */
+	private var matrix: FloatArray = FloatArray(9) { 0.111111f }
+
+	/**
+	 * Divisor - value by which we divide colors received during computations
+	 */
+	private var divisor: Float = 1f
+
+	/**
+	 * Width of matrix used in convolution filter
+	 *
+	 * @default: 3
+	 */
 	private var matrixWidth = 3
+
+	/**
+	 * Height of matrix used in convolution filter
+	 *
+	 * @default: 3
+	 */
 	private var matrixHeight = 3
+
+	/**
+	 * Offset - value added to each color
+	 *
+	 *  @default: 0
+	 */
 	private var offset = 0
+
+	/**
+	 * Array of changes in coordinates (looking from anchor point of view) which should be used for given matrix
+	 * e.g. [(x1) -1, (y1) -1, (x2) 0, (y2) -1, ...] means that for given (x, y) pixel we take following pixels into account
+	 * (x1, y1):(x+(-1), y+(-1)), (x2, y2):(x+(0), (y+(-1)), ...
+	 *
+	 * @default: 3x3 matrix with anchor in the middle
+	 */
+	private var changesInCoordinatesToConsider = intArrayOf(-1, -1, 0, -1, 1, -1, -1, 0, 0, 0, 1, 0, -1, 1, 0, 1, 1, 1)
 
 
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,12 +75,14 @@ class ConvolutionFiltersActivity : AppCompatActivity() {
 
 		rsContext = RenderScript.create(this)
 
-		initPixels()
-		setImageView()
+		setImageViewAndPixels()
 		initButtons()
 	}
 
-	private fun initPixels() {
+	/**
+	 * Initialize array of pixels using current bitmap from MainActivity
+	 */
+	private fun setPixels() {
 		var width = MainActivity.currentImage!!.width
 		var height = MainActivity.currentImage!!.height
 		var size = width * height
@@ -44,8 +91,9 @@ class ConvolutionFiltersActivity : AppCompatActivity() {
 		MainActivity.currentImage!!.getPixels(pixels, 0, width, 0, 0, width, height)
 	}
 
-	private fun setImageView() {
+	private fun setImageViewAndPixels() {
 		convolutionImageView.setImageBitmap(MainActivity.currentImage)
+		setPixels()
 	}
 
 	private fun initButtons() {
@@ -53,11 +101,10 @@ class ConvolutionFiltersActivity : AppCompatActivity() {
 	}
 
 
-
-	/* OTHER METHODS */
 	private fun toast(message: String) {
 		Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 	}
+
 
 	inner class BackgroundBlur : AsyncTask<Void, Void, Bitmap>() {
 
@@ -72,24 +119,32 @@ class ConvolutionFiltersActivity : AppCompatActivity() {
 			var aOut = Allocation.createFromBitmap(rsContext, bitmap)
 			val blur = ScriptC_blur(rsContext)
 			blur._bitmap_width = MainActivity.currentImage!!.width
-			blur._matrix_height = MainActivity.currentImage!!.height
+			blur._bitmap_height = MainActivity.currentImage!!.height
 			blur._matrix_width = matrixWidth
 			blur._matrix_height = matrixHeight
 			blur._offset = offset
+			blur._divisor = (matrixWidth * matrixHeight).toFloat() //in case of blur it's width*height
 
-			//assign pixels (from here) to *pixels in .rs
-			var intArrayBuilder = Type.Builder(rsContext, Element.I32(rsContext))
-			intArrayBuilder.setX(pixels!!.size)
-			var pxAlloc = Allocation.createTyped(rsContext, intArrayBuilder.create())
-			pxAlloc.copyFrom(pixels)
-			blur.bind_pixels(pxAlloc)
+			//assign pixels (from here) to *pixels in blur.rs
+			var pixelsBuilder = Type.Builder(rsContext, Element.I32(rsContext))
+			pixelsBuilder.setX(pixels!!.size)
+			var pixelsAlloc = Allocation.createTyped(rsContext, pixelsBuilder.create())
+			pixelsAlloc.copyFrom(pixels)
+			blur.bind_pixels(pixelsAlloc)
 
-			//assign divisors (from here) to *divisors in .rs
-			var floatArrayBuilder = Type.Builder(rsContext, Element.F32(rsContext))
-			floatArrayBuilder.setX(divisors!!.size)
-			var divAlloc = Allocation.createTyped(rsContext, floatArrayBuilder.create())
-			divAlloc.copyFrom(divisors)
-			blur.bind_divisors(divAlloc)
+			//assign changesInCoordinatesToConsider (from here) to *changes_in_coordinates_to_consider in blur.rs
+			var changesBuilder = Type.Builder(rsContext, Element.I32(rsContext))
+			changesBuilder.setX(changesInCoordinatesToConsider.size)
+			var changesAlloc = Allocation.createTyped(rsContext, changesBuilder.create())
+			changesAlloc.copyFrom(changesInCoordinatesToConsider)
+			blur.bind_changes_in_coordinates_to_consider(changesAlloc)
+
+			//assign matrix (from here) to *matrix in blur.rs
+			var matrixBuilder = Type.Builder(rsContext, Element.F32(rsContext))
+			matrixBuilder.setX(matrixHeight * matrixWidth)
+			var matrixAlloc = Allocation.createTyped(rsContext, matrixBuilder.create())
+			matrixAlloc.copyFrom(FloatArray(matrixHeight * matrixWidth) { 1f }) //in case of blur matrix consists of ones
+			blur.bind_matrix(matrixAlloc)
 
 			blur.forEach_blur(aIn, aOut)
 			aOut.copyTo(bitmap)
@@ -100,7 +155,7 @@ class ConvolutionFiltersActivity : AppCompatActivity() {
 		override fun onPostExecute(result: Bitmap?) {
 			toast("Blur is done")
 			MainActivity.currentImage = result
-			setImageView()
+			setImageViewAndPixels()
 		}
 	}
 }
--- app/src/main/rs/blur.rs
+++ app/src/main/rs/blur.rs
@@ -31,27 +31,41 @@ int matrix_height;
 /* Offset - value added to each color */
 int offset;
 
-/* Array of values of ranks assigned to given changes_in_coordinates_to_consider */
-float *divisors;
+/* Divisor - value by which we divide colors received during computations */
+float divisor;
+
+/* Array of values of weights assigned to given changes_in_coordinates_to_consider */
+float *matrix;
 
 
 uchar4 RS_KERNEL blur(uchar4 in, uint32_t x, uint32_t y) {
-	uchar4 out = in;
+	uchar4 out;
+	out.r = 0;
+	out.g = 0;
+	out.b = 0;
+	out.a = in.a;
 
-	/* DEBUG SECION */
 
-	//check using console if everything in import (*pixels) works properly
-	//if (x < 10 && y < 2) {
-    //    rsDebug("RS_TAG: values from function:", in);
+	/* DEBUG SECTION */
 
-	//	  int c = pixels[x + (y * bitmap_width)];
-	//    uchar4 out;
-    //    out.r = (c >> 16) & 0xFF; //take second 8 bits
-    //    out.g = (c >> 8) & 0xFF; //take third 8 bits
-    //    out.b = (c >> 0) & 0xFF; //take last 8 bits
-    //    out.a = (c >> 24) & 0xFF; //take first 8 bits
-	//    rsDebug("RS_TAG: values from array:", out);
-	//}
+//	if (x < 5 && y < 2) {
+//      rsDebug("RS_TAG: values from in:", in);
+//
+//		int c = pixels[x + (y * bitmap_width)];
+//	    uchar4 out;
+//		out.r = (c >> 16) & 0xFF; //take second 8 bits
+//		out.g = (c >> 8) & 0xFF; //take third 8 bits
+//      out.b = (c >> 0) & 0xFF; //take last 8 bits
+//      out.a = (c >> 24) & 0xFF; //take first 8 bits
+//	    rsDebug("RS_TAG: values from pixels:", out);
+//	    rsDebug("RS_TAG: ", 0);
+//
+//	    rsDebug("RS_TAG: first two values from changes:", changes_in_coordinates_to_consider[0], changes_in_coordinates_to_consider[1]);
+//	    rsDebug("RS_TAG: ", 0);
+//
+//	    rsDebug("RS_TAG: first two values from divisors", divisors[0], divisors[1]);
+//	    rsDebug("RS_TAG: ------------", 0);
+//	}
 
 
 
@@ -64,8 +78,60 @@ uchar4 RS_KERNEL blur(uchar4 in, uint32_t x, uint32_t y) {
 		int y_i = changes_in_coordinates_to_consider[(i * 2) + 1] + y;
 
 		//check if current x and y are in bounds of bitmap
-		if ((x < 0 || x > bitmap_width)){
-			;
+		if ((x_i < 0 || x_i > bitmap_width) || (y_i < 0 || y_i > bitmap_height)) {
+			//are out of bounds - use (x, y) from in
+
+			out.r += trunc((offset + (in.r * matrix[i])) / divisor);
+            //check bounds
+            if (out.r < 0)
+                out.r = 0;
+            else if (out.r > 255)
+            	out.r = 255;
+
+			out.g += trunc((offset + (in.g * matrix[i])) / divisor);
+			//check bounds
+			if (out.g < 0)
+				out.g = 0;
+			else if (out.g > 255)
+				out.g = 255;
+
+			out.b += trunc((offset + (in.b * matrix[i])) / divisor);
+			//check bounds
+			if (out.b < 0)
+				out.b = 0;
+			else if (out.b > 255)
+				out.b = 255;
+
+		} else {
+			//are in bounds - use (x_i, y_i)
+			int px = pixels[x_i + (y_i * bitmap_width)];
+
+			//get red color
+			int r = (px >> 16) & 0xFF;
+			out.r += trunc((offset + (r * matrix[i])) / divisor);
+			//check bounds
+			if (out.r < 0)
+				out.r = 0;
+			else if (out.r > 255)
+				out.r = 255;
+
+			//get green color
+			int g = (px >> 8) & 0xFF;
+			out.g += trunc((offset + (g * matrix[i])) / divisor);
+			//check bounds
+            if (out.g < 0)
+                out.g = 0;
+            else if (out.g > 255)
+            	out.g = 255;
+
+			//get blue color
+			int b = (px >> 0) & 0xFF;
+			out.b += trunc((offset + (b * matrix[i])) / divisor);
+			//check bounds
+            if (out.b < 0)
+            	out.b = 0;
+            else if (out.b > 255)
+            	out.b = 255;
 		}
 	}
 

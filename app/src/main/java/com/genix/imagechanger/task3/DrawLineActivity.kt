220673a7dbf49cdbf834d6195adb22a6bd7aefb0
Łukasz Niedziałek <intergenixyt@gmail.com>
2019-04-10 20:13:21 +0200

    draw straight lines

--- app/src/main/java/com/genix/imagechanger/task3/DrawLineActivity.kt
+++ app/src/main/java/com/genix/imagechanger/task3/DrawLineActivity.kt
@@ -28,6 +28,8 @@ class DrawLineActivity : AppCompatActivity() {
 		setImageView()
 		initButtons()
 		initEditTexts()
+
+		midpointLine(20, 200, 200, 20)
 	}
 
 	private fun initEditTexts() {
@@ -126,37 +128,117 @@ class DrawLineActivity : AppCompatActivity() {
 	}
 
 	private fun initButtons() {
-		applyN1Button.setOnClickListener { midpointLine(x1, y1, x2, y2) }
+			applyN1Button.setOnClickListener { drawLine() }
 	}
 
 	private fun setImageView() {
 		drawLineImageView.setImageBitmap(MainActivity.currentImage)
+
+		drawLineImageView.setOnClickListener {
+			toast("($x1, $y1) \n ($x2 $y2)")
+		}
+	}
+
+	private fun drawLine() {
+		if (x1 == x2) {
+			drawVerticalLine(x1, y1, x2, y2)
+			return
+		}
+
+		if (y1 == y2) {
+			drawHorizontalLine(x1, y1, x2, y2)
+			return
+		}
+
+		if (x1 < x2) {
+			if (y1 < y2) {
+
+				return
+			} else {
+
+				return
+			}
+		}
+
+		if (x1 > x2) {
+			if (y1 < y2) {
+
+				return
+			} else {
+
+				return
+			}
+		}
+	}
+
+	private fun drawVerticalLine(x1: Int, y1: Int, x2: Int, y2: Int) {
+		var smaller: Int
+		var bigger: Int
+		var img: Bitmap = MainActivity.currentImage!!.copy(Bitmap.Config.ARGB_8888,true)
+
+		if (y1 < y2) {
+			smaller = y1
+			bigger = y2
+		} else {
+			smaller = y2
+			bigger = y1
+		}
+
+		while (smaller < bigger) {
+			putPixel(img, x1, smaller)
+			smaller++
+		}
+
+		MainActivity.currentImage = img
+		setImageView()
+	}
+
+	private fun drawHorizontalLine(x1: Int, y1: Int, x2: Int, y2: Int) {
+		var smaller: Int
+		var bigger: Int
+		var img: Bitmap = MainActivity.currentImage!!.copy(Bitmap.Config.ARGB_8888,true)
+
+		if (x1 < x2) {
+			smaller = x1
+			bigger = x2
+		} else {
+			smaller = x2
+			bigger = x1
+		}
+
+		while (smaller < bigger) {
+			putPixel(img, smaller, y1)
+			smaller++
+		}
+
+		MainActivity.currentImage = img
+		setImageView()
 	}
 
 	private fun midpointLine(x1: Int, y1: Int, x2: Int, y2: Int) {
 		val dx = x2 - x1
 		val dy = y2 - y1
-		var d = 2 * dy - dx
-		val dE = 2 * dy
-		val dNE = 2 * (dy - dx)
+
+		var d = dy - dx/2 //initial value for decision parameter
+
 		var x = x1
-		var y = y1
+		var y = MainActivity.currentImage!!.height - y1 - 1
 
 		var img: Bitmap = MainActivity.currentImage!!.copy(Bitmap.Config.ARGB_8888,true)
 
-		putPixel(img, x, y)
+		putPixel(img, x, MainActivity.currentImage!!.height - y)
 		while (x < x2) {
+			x++
+
 			if (d < 0) { // move to E
-				d += dE
-				x++
+				d += dy
 
 			} else { // move to NE
-				d += dNE
-				++x
-				++y
+				d += d - dy + dx
+				y++
 			}
 
-			putPixel(img, x, y)
+			putPixel(img, x, MainActivity.currentImage!!.height - y - 1)
 		}
 
 		MainActivity.currentImage = img
